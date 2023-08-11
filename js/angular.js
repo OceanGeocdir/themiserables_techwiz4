34dbf024eaf23cfa1f331f3dbd0ec574ea127544
jortboy2 <vonghung849@gmail.com>
2023-08-11 23:38:52 +0700

    321321
    Co-Authored-By: lekhuyen <127919257+lekhuyen@users.noreply.github.com>
    Co-Authored-By: nghimathit <128113915+nghimathit@users.noreply.github.com>
    Co-Authored-By: Pearlicia <127919091+pearliciahu@users.noreply.github.com>
    Co-Authored-By: DuyHuynh <127919188+duymonster99@users.noreply.github.com>
    Co-Authored-By: DANG DAI DUONG <127579534+OceanGeocdir@users.noreply.github.com>

--- js/angular.js
+++ js/angular.js
@@ -1,14 +1,14 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
-    $routeProvider  
+  $routeProvider  
     .when("/", { templateUrl: "home.html"})
     .when("/home", { templateUrl: "home.html"})
     .when("/about", { templateUrl: "about.html"})
     .when("/cart", { templateUrl: "newcart.html", controller: "CartController" })
     .when("/contact", { templateUrl: "contact.html"})
     .when("/feedback", { templateUrl: "feedback.html"})
-    .when("/product", { templateUrl: "product.html"})
+    .when("/product", { templateUrl: "product.html", controller: "productController" })
     .when("/login", { templateUrl: "login.html"})
 });
 
@@ -45,7 +45,59 @@ app.controller("feedbackController", function ($scope, $http){
 });
 
 app.controller("productController", function ($scope, $http){
-    // trang product.html
+
+    var app = angular.module("myApp", ["ngRoute"]);
+
+    app.config(function ($routeProvider) {
+      $routeProvider
+      .when("/", { templateUrl: "page1.html" })  // Không cần controller ở đây
+      .when("/a", { templateUrl: "page1.html" })   // Cấu hình routing đến page1.html
+      .when("/b", { templateUrl: "page2.html" })   // Cấu hình routing đến page2.html
+      .when("/c", { templateUrl: "page3.html" })   // Cấu hình routing đến page3.html
+      .when("/d", { templateUrl: "page4.html" })   // Cấu hình routing đến page4.html
+    });
+    
+    app.controller("productController", function ($scope, $http) {
+      $http.get("./json/Succulents2.json").then(function (response) {
+        $scope.data1 = response.data.page1;
+        $scope.data2 = response.data.page2;
+        $scope.data3 = response.data.page3;
+        $scope.data4 = response.data.page4;
+        
+      });
+      $scope.addToCart = function (item) {
+        var productInfo = {
+          id: item.id,
+          product: item.name,
+          price: item.price,
+          img: item.img,
+        };
+    
+        var productList = JSON.parse(localStorage.getItem("product_list")) || [];
+    
+        var existingProduct = productList.find(function (product) {
+          return product.id === item.id;
+        });
+    
+        if (existingProduct) {
+          existingProduct.quantity += 1;
+        } else {
+          productInfo.quantity = 1;
+          productList.push(productInfo);
+        }
+    
+        localStorage.setItem("product_list", JSON.stringify(productList));
+      };
+    });
+    
+    
+    
+    //filter
+    
+    //sort
+    
+    //search
+    
 });
 
 app.controller("loginController", function ($scope, $http){
--- js/product.js
+++ js/product.js
@@ -2,134 +2,20 @@ var app = angular.module("myApp", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
   $routeProvider
-    .when("/", {
-      templateUrl: "/page/page1.html",
-      controller: "page1Controller",
-    })
-    .when("/a", {
-      templateUrl: "/page/page1.html",
-      controller: "page1Controller",
-    })
-    .when("/b", {
-      templateUrl: "/page/page2.html",
-      controller: "page2Controller",
-    })
-    .when("/c", {
-      templateUrl: "/page/page3.html",
-      controller: "page3Controller",
-    })
-    .when("/d", {
-      templateUrl: "/page/page4.html",
-      controller: "page4Controller",
-    })
-    .when("/e", {
-      templateUrl: "/page/page5.html",
-      controller: "page5Controller",
-    })
-    .when("/f", {
-      templateUrl: "/page/page6.html",
-      controller: "page6Controller",
-    })
-    .when("/g", {
-      templateUrl: "/page/page7.html",
-      controller: "page7Controller",
-    })
-    .when("/h", {
-      templateUrl: "/page/page8.html",
-      controller: "page8Controller",
-    });
+  .when("/", { templateUrl: "page1.html" })  // Không cần controller ở đây
+  .when("/a", { templateUrl: "page1.html" })   // Cấu hình routing đến page1.html
+  .when("/b", { templateUrl: "page2.html" })   // Cấu hình routing đến page2.html
+  .when("/c", { templateUrl: "page3.html" })   // Cấu hình routing đến page3.html
+  .when("/d", { templateUrl: "page4.html" })   // Cấu hình routing đến page4.html
 });
 
-app.controller("page1Controller", function ($scope, $http) {
+app.controller("productController", function ($scope, $http) {
   $http.get("./json/Succulents2.json").then(function (response) {
     $scope.data1 = response.data.page1;
-  });
-  $scope.addToCart = function (item) {
-    var productInfo = {
-      id: item.id,
-      product: item.name,
-      price: item.price,
-      img: item.img,
-    };
-
-    var productList = JSON.parse(localStorage.getItem("product_list")) || [];
-
-    var existingProduct = productList.find(function (product) {
-      return product.id === item.id;
-    });
-
-    if (existingProduct) {
-      existingProduct.quantity += 1;
-    } else {
-      productInfo.quantity = 1;
-      productList.push(productInfo);
-    }
-
-    localStorage.setItem("product_list", JSON.stringify(productList));
-  };
-});
-
-app.controller("page2Controller", function ($scope, $http) {
-  $http.get("./json/Succulents2.json").then(function (response) {
     $scope.data2 = response.data.page2;
-  });
-  $scope.addToCart = function (item) {
-    var productInfo = {
-      id: item.id,
-      product: item.name,
-      price: item.price,
-      img: item.img,
-    };
-
-    var productList = JSON.parse(localStorage.getItem("product_list")) || [];
-
-    var existingProduct = productList.find(function (product) {
-      return product.id === item.id;
-    });
-
-    if (existingProduct) {
-      existingProduct.quantity += 1;
-    } else {
-      productInfo.quantity = 1;
-      productList.push(productInfo);
-    }
-
-    localStorage.setItem("product_list", JSON.stringify(productList));
-  };
-});
-
-app.controller("page3Controller", function ($scope, $http) {
-  $http.get("./json/Succulents2.json").then(function (response) {
     $scope.data3 = response.data.page3;
-  });
-  $scope.addToCart = function (item) {
-    var productInfo = {
-      id: item.id,
-      product: item.name,
-      price: item.price,
-      img: item.img,
-    };
-
-    var productList = JSON.parse(localStorage.getItem("product_list")) || [];
-
-    var existingProduct = productList.find(function (product) {
-      return product.id === item.id;
-    });
-
-    if (existingProduct) {
-      existingProduct.quantity += 1;
-    } else {
-      productInfo.quantity = 1;
-      productList.push(productInfo);
-    }
-
-    localStorage.setItem("product_list", JSON.stringify(productList));
-  };
-});
-
-app.controller("page4Controller", function ($scope, $http) {
-  $http.get("./json/Succulents2.json").then(function (response) {
     $scope.data4 = response.data.page4;
+    
   });
   $scope.addToCart = function (item) {
     var productInfo = {
@@ -156,34 +42,7 @@ app.controller("page4Controller", function ($scope, $http) {
   };
 });
 
-app.controller("page5Controller", function ($scope, $http) {
-  $http.get("./json/Succulents2.json").then(function (response) {
-    $scope.data5 = response.data.page5;
-  });
-  $scope.addToCart = function (item) {
-    var productInfo = {
-      id: item.id,
-      product: item.name,
-      price: item.price,
-      img: item.img,
-    };
 
-    var productList = JSON.parse(localStorage.getItem("product_list")) || [];
-
-    var existingProduct = productList.find(function (product) {
-      return product.id === item.id;
-    });
-
-    if (existingProduct) {
-      existingProduct.quantity += 1;
-    } else {
-      productInfo.quantity = 1;
-      productList.push(productInfo);
-    }
-
-    localStorage.setItem("product_list", JSON.stringify(productList));
-  };
-});
 
 //filter
 
